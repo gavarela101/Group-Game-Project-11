90f32f78dba4df78241fe167f4f58f2e175c19e8
XabielG <xabielgarcia2006@gmail.com>
2025-05-12 22:47:45 -0700

    Upd

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerController PlayerController;
+    public TMP_Text healthText;
+    public TMP_Text livesText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthText.text = "Health: " + PlayerController.health;
+        livesText.text = "Lives: " + PlayerController.lives;
     }
 }
