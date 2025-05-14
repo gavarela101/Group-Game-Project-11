0d8f075dfa3e41b7a5c2c23020402af35c3eb152
gavarela101 <gabrielvarela055@gmail.com>
2025-05-13 23:06:41 -0700

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,13 +10,17 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    public Transform portalExit;
+    public GameObject playerPrefab; // Assign the player prefab in the inspector
+    public Transform respawnPoint; // Assign the respawn point in the inspector
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            other.transform.position = portalExit.transform.position;
+            // Destroy the player
+            Destroy(other.gameObject);
+            // Spawn the player at the respawn point
+            Instantiate(playerPrefab, respawnPoint.position, respawnPoint.rotation);
         }
     }
 }
